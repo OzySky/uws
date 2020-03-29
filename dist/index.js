77375f0e8b3f1a342b7803140ac736784a56f13d
Ozy Sky <ozydesign1@gmail.com>
2020-03-29 21:22:54 +0300

    Added Server export like old uws api

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var client_1 = require("./client");
 exports.WebSocket = client_1.WebSocket;
 var server_1 = require("./server");
-exports.WebSocketServer = server_1.WebSocketServer;
+exports.Server = exports.WebSocketServer = server_1.WebSocketServer;
 exports.secureProtocol = 'TLSv1_2_method';
